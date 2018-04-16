6aba41658236f8771cb2eeb2e1d8712dac270ca4
Allan Adair <allan.adair@geodata.no>
2018-04-16 15:52:30 +0200

    tmpOnTmpfs = true

--- scripts/configuration.nix
+++ scripts/configuration.nix
@@ -10,6 +10,7 @@
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
   boot.loader.grub.device = "/dev/sda";
+  boot.tmpOnTmpfs = true;
 
   # remove the fsck that runs at startup. It will always fail to run, stopping
   # your boot until you press *. 
